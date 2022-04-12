c66a59463a68588c058b237f2fa296797dbf02b8
agrant216 <agrant12@kent.edu>
2022-04-12 10:29:52 -0400

    Adding mocked api

--- src/app/services/player-character.service.ts
+++ src/app/services/player-character.service.ts
@@ -2,11 +2,13 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { PlayerCharacter } from "src/app/models/player-character.model";
 
+
 @Injectable({providedIn: "root"})
 export class PlayerCharacterService {
   constructor(private http: HttpClient){}
 
   public getNewCharacter() {
-    return this.http.get<PlayerCharacter>('https://localhost:7092/api/playercharacter/new');
+    //return this.http.get<PlayerCharacter>('https://localhost:7092/api/playercharacter/new');
+    return this.http.get<PlayerCharacter>('assets/dummy.json');
   }
 }
